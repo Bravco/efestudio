27cd68a556857212b62be8bd4f97a9894ec0e04f
Bravco <boss.wiw@gmail.com>
2025-06-10 17:43:46 +0200

    Updated error page

--- error.vue
+++ error.vue
@@ -1,8 +1,9 @@
 <template>
     <NuxtLayout>
         <div>
-            <section class="min-h-dvh flex items-end">
-                <h1 style="font-size: clamp(64px, 15vw, 360px);">{{ error.statusCode }} ERROR</h1>
+            <section class="min-h-dvh flex flex-col justify-end">
+                <span class="md:text-[62px] text-[32px]">STRÁNKA NENÍ DOSTUPNÁ</span>
+                <h1>{{ error.statusCode }}</h1>
             </section>
         </div>
     </NuxtLayout>
