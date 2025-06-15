6b6059c6ebd7838ca485a826c2accb623f4161dd
Bravco <boss.wiw@gmail.com>
2025-06-15 17:43:02 +0200

    Upscaled partners logos

--- components/Partners.vue
+++ components/Partners.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-px">
         <div class="outline outline-1 outline-black aspect-square grid place-items-center">
-            <NuxtImg height="48" src="/images/nario-logo.webp" alt="nario" />
+            <NuxtImg width="140" height="48" src="/images/nario-logo.webp" alt="nario" />
         </div>
         <div class="outline outline-1 outline-black aspect-square grid place-items-center">
-            <NuxtImg height="48" src="/images/lavarch-logo.webp" alt="lavarch" />
+            <NuxtImg width="165" height="45" src="/images/lavarch-logo.webp" alt="lavarch" />
         </div>
         <div class="outline outline-1 outline-black aspect-square grid place-items-center">
-            <NuxtImg height="48" src="/images/bagrespis-logo.webp" alt="bagrespis" />
+            <NuxtImg width="197" height="43" src="/images/bagrespis-logo.webp" alt="bagrespis" />
         </div>
         <div class="outline outline-1 outline-black aspect-square grid place-items-center">
-            <NuxtImg height="48" src="/images/galikin-logo.webp" alt="galikin" />
+            <NuxtImg width="181" height="44" src="/images/galikin-logo.webp" alt="galikin" />
         </div>
         <div class="outline outline-1 outline-black aspect-square grid place-items-center">
-            <NuxtImg width="96" src="/images/marmat-logo.webp" alt="marmat" />
+            <NuxtImg width="103" height="81" src="/images/marmat-logo.webp" alt="marmat" />
         </div>
     </div>
 </template>
