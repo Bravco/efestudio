b17b403fb5e426138de6a311cb2ac09fc50e6696
Bravco <boss.wiw@gmail.com>
2025-06-19 15:02:03 +0200

    Added nuxt robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
-    "@nuxt/content",
-    "lenis/nuxt",
-    "v-gsap-nuxt"
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/content", "lenis/nuxt", "v-gsap-nuxt", "@nuxtjs/robots"],
   css: [ "~/assets/main.css" ],
   app: {
     head: {
@@ -23,10 +17,6 @@ export default defineNuxtConfig({
         {
           name: "description",
           content: "Sme digitálne marketingové štúdio. Klientom prinášame komplexné riešenia v oblasti značky obsahu a webov. Každý projekt staviame na jasnej stratégii a cieľoch.",
-        },
-        {
-          name: "robots",
-          content: "index, follow",
         }
       ]
     }
