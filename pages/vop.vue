e97340d632b33afa3479168663a32b19897b2bb0
Bravco <boss.wiw@gmail.com>
2025-06-28 19:29:29 +0200

    fixed vop hydratation error

--- pages/vop.vue
+++ pages/vop.vue
@@ -5,14 +5,13 @@
             <h2>(01) Úvodné ustanovenia</h2>
             <p>Tieto Všeobecné obchodné podmienky (ďalej len „VOP“) upravujú právne vzťahy medzi spoločnosťou efestudio s.r.o., IČO: 55796231, so sídlom Dubová 4, 811 04 Bratislava - mestská časť Staré Mesto, zapísanou v Obchodnom registri SR (ďalej len „Poskytovateľ“) a jej zákazníkmi (ďalej len „Klient“), v súvislosti s poskytovaním služieb digitálneho marketingu.</p>
             <h2>(02) Predmet poskytovaných služieb</h2>
-            <p>Poskytovateľ poskytuje klientom najmä nasledovné služby:
-                <ul>
-                    <li>Tvorba a správa webových stránok a e-shopov</li>
-                    <li>Online marketing (PPC, SEO, e-mail marketing, sociálne siete)</li>
-                    <li>Grafický dizajn a branding</li>
-                    <li>Konzultácie v oblasti digitálneho marketingu</li>
-                </ul>
-            </p>
+            <ul>
+                <span>Poskytovateľ poskytuje klientom najmä nasledovné služby:</span>
+                <li>Tvorba a správa webových stránok a e-shopov</li>
+                <li>Online marketing (PPC, SEO, e-mail marketing, sociálne siete)</li>
+                <li>Grafický dizajn a branding</li>
+                <li>Konzultácie v oblasti digitálneho marketingu</li>
+            </ul>
         </section>
     </div>
 </template>
@@ -24,5 +23,9 @@
 
     h2 {
         font-size: 24px;
+    }
+
+    ul, li {
+        list-style: disc;
     }
 </style>
