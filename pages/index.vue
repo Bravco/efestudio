b1220b907b6f6954f36c4e3a74d542dd173dd9f5
Bravco <boss.wiw@gmail.com>
2025-07-12 12:07:05 +0200

    Removed projects text button

--- pages/index.vue
+++ pages/index.vue
@@ -36,11 +36,7 @@
 
         <section class="bg-[var(--color-white)] flex flex-col gap-4">
             <div class="grid grid-cols-2 gap-6">
-                <NuxtLink class="flex items-center gap-2" to="/works">
-                    Viac projektov
-                    <NuxtImg src="/images/arrow.svg" height="16" width="16" alt="more projects"/>
-                </NuxtLink>
-                <h2 class="small-alt-text">(VYBRANÉ PROJEKTY)</h2>
+                <h2 class="small-alt-text col-start-2">(VYBRANÉ PROJEKTY)</h2>
             </div>
             <div class="grid md:grid-cols-2 grid-cols-1 gap-x-6 gap-y-12">
                 <ProjectCard v-for="project in projects" :key="project.title" :project="project"/>
